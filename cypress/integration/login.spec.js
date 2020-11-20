c2e7c561730311f17c9883448c554a5a3199e71d
Igor Gnidko <ignidko@solvd.com>
2020-11-20 15:16:30 +0200

    more tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -10,4 +10,17 @@ describe ('log in suite', () => {
         cy.get('.side-menu__toggler > svg').click()
         cy.contains('Войти в').click().should('contain', 'Войти')
     })
+
+    it.only('side menu test', ()=>{
+        cy.get('.side-menu__toggler > svg').click()
+        cy.contains('Город').get('.menu-location > rz-city > .button--link').click()
+        cy.get('.autocomplete__input').clear().type('Черновцы')
+        cy.get('button').should('contain','Применить')
+    })
+
+    it('basket test', ()=>{
+        cy.get('.js-rz-cart > .header-actions__button-wrapper > .header-actions__button').click()
+        cy.contains('Корзина').should('contain', 'Корзина ')
+        cy.get('.cart-dummy').parents('.cart').get('.cart-dummy__heading').should('contain','Корзина пуста')
+    })
 })
